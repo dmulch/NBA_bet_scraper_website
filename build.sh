281013c2d1e33cdbe47b9e3e92ac93c7b8087cfc
Devin Mulcahy <dmulcahy24@gsb.columbia.edu>
2023-01-17 16:48:16 -0500

    comment out echo from build.sh

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin1',
-'admin@example.com', 'pass')" | python manage.py shell
+#echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin1',
+#'admin@example.com', 'pass')" | python manage.py shell
