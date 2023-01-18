18057749a8c755df95628a1d0a685b7f4ac10850
JustinPedley <jpedley24@gsb.columbia.edu>
2023-01-18 14:04:37 -0500

    comment test

--- project1/settings.py
+++ project1/settings.py
@@ -130,3 +130,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")] #used for dealing w/ image
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Justin Comment Test
