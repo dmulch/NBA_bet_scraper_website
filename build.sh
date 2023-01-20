dd97ae8c23c811b1f4789a79cf3a0d0989f8d5ad
Devin Mulcahy <dmulcahy24@gsb.columbia.edu>
2023-01-20 10:28:23 -0500

    trying superuser creation

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
+printf "admin\ndmulcahy24@gsb.columbia.edu\n1234qwer1234qwer" | python manage.py createsuperuser
 #echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin1',
 #'admin@example.com', 'pass')" | python manage.py shell
